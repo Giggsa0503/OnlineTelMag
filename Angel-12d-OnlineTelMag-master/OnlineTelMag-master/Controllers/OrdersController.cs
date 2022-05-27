e45bcfd30c9f04a402d3abee67a093d50f0289a3
Ангел <angel.sngelov0503@gmail.com>
2022-05-27 22:20:16 +0300

    wsichko raboti

---
+++ Angel-12d-OnlineTelMag-master/OnlineTelMag-master/Controllers/OrdersController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineTelMag.Data;
+
+namespace OnlineTelMag.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+        public OrdersController(ApplicationDbContext context,
+                                                            UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Test()
+        {
+            var userLoged = await _userManager.GetUserAsync(User);
+            var result = await _userManager.AddToRoleAsync(userLoged, Roles.Admin.ToString());   //"Admin");
+            var roles = _userManager.GetRolesAsync(userLoged);
+            return Content("OK !!!");
+        }
+        // GET: Orders
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var applicationDbContext = _context.Orders
+                .Include(o => o.Telephones)
+                .Include(o => o.Users);
+                return View(await applicationDbContext.ToListAsync());
+            }
+            else
+            {
+                var currentUser = _userManager.GetUserId(User);
+                var myOrders = _context.Orders
+                               .Include(o => o.Telephones)
+                               .Include(u => u.Users)
+                               .Where(x => x.UserId == currentUser.ToString())
+                               .ToListAsync();
+
+                return View(await myOrders);
+            }
+
+        }
+        // GET: Orders
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index1()
+        {
+            var applicationDbContext = _context.Orders
+                .Include(o => o.Telephones)
+                .Include(o => o.Users);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Telephones)
+                .Include(o => o.Users)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["TelephoneId"] = new SelectList(_context.Telephones, "Id", "TelName");
+
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TelephoneId")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.UserId=_userManager.GetUserId(User) ;
+                order.DateRegister = DateTime.Now;
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TelephoneId"] = new SelectList(_context.Telephones, "Id", "TelName", order.TelephoneId);
+            
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["TelephoneId"] = new SelectList(_context.Telephones, "Id", "TelName", order.TelephoneId);
+            
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TelephoneId")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    order.UserId = _userManager.GetUserId(User);
+                    order.DateRegister = DateTime.Now;
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TelephoneId"] = new SelectList(_context.Telephones, "Id", "TelName", order.TelephoneId);
+            
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Telephones)
+                .Include(o => o.Users)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Angel-12d-OnlineTelMag-master/OnlineTelMag-master/Controllers/TelephonesController.cs
@@ -0,0 +1,238 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineTelMag.Data;
+using OnlineTelMag.Models;
+
+namespace OnlineTelMag.Controllers
+{
+    public class TelephonesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private string wwwroot;
+
+        public TelephonesController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+            wwwroot = $"{this._hostEnvironment.WebRootPath}";
+        }
+
+        // GET: Telephones
+        public async Task<IActionResult> Index()
+        {
+            List<Telephone> model = await _context.Telephones
+                .Include(t => t.Brands)
+                .Include(img => img.TelephoneImages)
+                .ToListAsync();
+            foreach (var item in model)
+            {
+                item.TelephoneImages = _context.TelephonesImages.Where(x => x.TelephoneId == item.Id).ToList();
+            }
+
+            return View(model);
+            
+        }
+        public async Task<IActionResult> Index1()
+        {
+            List<Telephone> model = await _context.Telephones
+                .Include(t => t.Brands)
+                .Where(x => x.BrandId == 12)
+                .Include(img => img.TelephoneImages)
+                .ToListAsync();
+            foreach (var item in model)
+            {
+                item.TelephoneImages = _context.TelephonesImages.Where(x => x.TelephoneId == item.Id).ToList();
+            }
+            
+            return View(model);
+        }
+
+
+        // GET: Telephones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Telephone telephone = await _context.Telephones
+                .Include(img => img.TelephoneImages)
+                .Include(t => t.Brands)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+
+            TelephoneDetailsVM modelVM = new TelephoneDetailsVM();
+            modelVM.TelName = telephone.TelName;
+            modelVM.Prise = telephone.Prise;
+            modelVM.Description = telephone.Description;
+            modelVM.Date = telephone.Date;
+            modelVM.Color = telephone.Color;
+            modelVM.Broi = telephone.Broi;
+            modelVM.BrandId = telephone.BrandId;
+            modelVM.ImagesPaths = _context.TelephonesImages
+                .Where(img => img.TelephoneId == telephone.Id)
+                .Select(x => $"/Images/{x.ImagePath}").ToList<string>();
+            
+            return View(modelVM);
+        }
+
+        // GET: Telephones/Create
+        [Authorize (Roles="Admin")]
+        public IActionResult Create()
+        { 
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "NameBrand");
+            return View();
+        }
+ // var imagePath = Path.Combine(wwwroot, "Images");//modelVM.Brands = _context.Brands
+ //.Where(x => x.Id == modelVM.BrandId).ToList<SelectListItem>();
+
+        // POST: Telephones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] TelephoneVM telephone)
+        {
+            if (!ModelState.IsValid)
+            {
+                telephone.Date = DateTime.Now;
+                ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "NameBrand", telephone.BrandId);
+                return View(telephone);
+            }
+            ImagesBuilder imagesBuilder = new ImagesBuilder(_context, _hostEnvironment);
+            await imagesBuilder.CreateImages(telephone);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Telephones/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telephone = await _context.Telephones.FindAsync(id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+            TelephoneDetailsVM modelVM = new TelephoneDetailsVM();
+            //modelVM.Id = (int)id;
+            modelVM.TelName = telephone.TelName;
+            modelVM.Prise = telephone.Prise;
+            modelVM.Description = telephone.Description;
+            modelVM.Date = telephone.Date;
+            modelVM.Color = telephone.Color;
+            modelVM.Broi = telephone.Broi;
+            modelVM.BrandId = telephone.BrandId;
+            modelVM.ImagesPaths = _context.TelephonesImages
+                .Where(img => img.TelephoneId == telephone.Id)
+                .Select(x => $"/Images/{x.ImagePath}").ToList<string>();
+            modelVM.Brands = _context.Brands.Select(x => new SelectListItem
+            {
+                Text = x.NameBrand,
+                Value = x.Id.ToString(),
+                Selected=(x.Id==modelVM.BrandId)
+
+            }).ToList();
+
+            //ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "NameBrand", telephone.BrandId);
+            return View(modelVM);
+        }
+
+        // POST: Telephones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TelName,BrandId,Prise,Description,Color,Broi,Date")] TelephoneDetailsVM telephone)
+        {
+
+            if (!ModelState.IsValid)
+            {
+               return RedirectToAction(nameof(Index));
+            }
+            Telephone telefoneToDB =await _context.Telephones.FindAsync(id);
+            telefoneToDB.TelName = telephone.TelName;
+            telefoneToDB.Prise = telephone.Prise;
+            telefoneToDB.Description = telephone.Description;
+            telefoneToDB.Date = telephone.Date;
+            telefoneToDB.Color = telephone.Color;
+            telefoneToDB.Broi = telephone.Broi;
+            telefoneToDB.BrandId = telephone.BrandId;
+            
+            try
+            {
+                telefoneToDB.Date = DateTime.Now;
+                _context.Update(telefoneToDB);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TelephoneExists(telefoneToDB.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+           
+            return RedirectToAction("Details", new { id = id });
+        }
+
+        // GET: Telephones/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telephone = await _context.Telephones
+                .Include(t => t.Brands)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+
+            return View(telephone);
+        }
+
+        // POST: Telephones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var telephone = await _context.Telephones.FindAsync(id);
+            _context.Telephones.Remove(telephone);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TelephoneExists(int id)
+        {
+            return _context.Telephones.Any(e => e.Id == id);
+        }
+    }
+}
